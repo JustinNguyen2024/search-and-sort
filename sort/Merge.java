51927f630ce9a5149a0ce7ba3292d160626fb283
Justin Nguyen <justin.nguyen@mrjaffesclass.com>
2023-09-13 12:44:32 -0700

    finished all

--- sort/Merge.java
+++ sort/Merge.java
@@ -10,69 +10,77 @@ import java.util.Arrays;
  */
 public class Merge
 {
+    void merge(int arr[], int left, int mid, int right)
+    {
+        // Find sizes of two subarrays to be merged
+        int lSize = mid - left + 1;
+        int rSize = right - mid;
+
+        // Create temp arrays
+        int lArray[] = new int[lSize];
+        int rArray[] = new int[rSize];
+
+        // put values into temp arrays
+        for (int i = 0; i < lSize; ++i)
+            lArray[i] = arr[left + i];
+        for (int j = 0; j < rSize; ++j)
+            rArray[j] = arr[mid + 1 + j];
+
+        // Initial indices of first and second subarrays
+        int lPointer = 0, rPointer = 0;
+
+        // merge and sort starting from bottom
+        int k = left;
+        while (lPointer < lSize && rPointer < rSize) {
+            if (lArray[lPointer] <= rArray[rPointer]) {
+                arr[k] = lArray[lPointer];
+                lPointer++;
+            }
+            else {
+                arr[k] = rArray[rPointer];
+                rPointer++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (lPointer < lSize) {
+            arr[k] = lArray[lPointer];
+            lPointer++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (rPointer < rSize) {
+            arr[k] = rArray[rPointer];
+            rPointer++;
+            k++;
+        }
+    }
+
+    public void sortArr(int arr[], int l, int r) {
+        if (l < r) {
+
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
     /**
      * Returns the sorted array
      */
-    public static int[] sort(int[] arr, int lArray, int rArray) {
-        if (lArray < rArray-1) {
-            int middle = lArray + (rArray-lArray) / 2;
-
-            sort(arr, lArray, middle);
-            sort(arr, middle, rArray);
-            merge(arr, lArray, middle, rArray);
-        }
+    public static int[] sort(int arr[], int l, int r)
+    {
+        Merge mergeSort = new Merge();
+        mergeSort.sortArr(arr, l, r);
         return arr;
-    }
-
-    private static void merge(int[] arr, int lArray, int middle, int rArray) {
-        int length = arr.length;
-
-        // create 2 new sub arrays
-        int[] leftArray = new int[middle];
-        int[] rightArray = new int[length - middle];
-
-        // put elements in left and right arrays
-        int l = 0;
-        int r = 0;
-
-        for (; l < length; l++) {
-            if (l < middle) {
-                leftArray[l] = arr[l];
-            } else {
-                rightArray[r] = arr[l];
-                r++;
-            }
-        }
-
-        //part 2: put back together
-        int leftSize = middle - l + 1;
-        int rightSize = r - middle;
-        int i = 0, l2 = 0, r2 = 0; // indices
-        int rightArraySize = rightArray.length;
-        // merge smaller # first
-        while (l2 < leftSize && r2 < rightSize) {
-            if(leftArray[l2] < rightArray[r2]) {
-                arr[i] = leftArray[l2];
-                i++;
-                l2++;
-            } else {
-                arr[i] = rightArray[r2];
-                i++;
-                r2++;
-            }
-        }
-
-        // merge last element up if only one remains
-        while(l2 < leftSize) {
-            arr[i] = leftArray[l2];
-            i++;
-            l2++;
-        }
-        while(r2 < rightSize) {
-            arr[i] = rightArray[r2];
-            i++;
-            r++;
-        }
     }
 
     public static void main(String[] args) {
@@ -82,6 +90,9 @@ public class Merge
 
         // Test the sort
         testSort(sort(arr, 0, arr.length - 1));
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
     }
 
     public static void testSort(int[] arr) {
